da6f7da779fabf52e4c41cea428e66ed0f797e4f
Eric Ma <ericmajinglong@gmail.com>
2015-07-26 23:57:39 -0400

    added initial setup.py file and setup.cfg

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='flu-gibson',
+    packages=['flu-gibson'],  # this must be the same as the name above
+    version='1.0',
+    description='A Python package for designing influenza reverse genetics \
+                 primers using the seamless cloning methods (e.g. Gibson \
+                 assembly, CPEC assembly).',
+    author='Eric J. Ma',
+    author_email='ericmajinglong@gmail.com',
+    # use the URL to the github repo
+    url='https://github.com/ericmjl/flu-gibson',
+    keywords=['biology'],  # arbitrary keywords
+    classifiers=[],
+)
