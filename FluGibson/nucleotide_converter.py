f86989fedd1f6fd4747e764b827a79882706ce12
Eric Ma <ericmajinglong@gmail.com>
2015-08-11 15:24:00 -0400

    finished nucleotide assembly computations

--- FluGibson/nucleotide_converter.py
+++ FluGibson/nucleotide_converter.py
@@ -5,7 +5,7 @@ Date: 11 August 2015
 
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-from Bio.Aplhabet import DNAAlphabet
+from Bio.Alphabet import generic_dna
 from collections import defaultdict
 from itertools import combinations
 import networkx as nx
@@ -18,7 +18,7 @@ class NucleotideConverter(object):
     needed to convert the first one to the other.
     """
 
-    def __init__(self, source, destination):
+    def __init__(self):
         super(NucleotideConverter, self).__init__()
 
         # The source sequence.
@@ -38,55 +38,51 @@ class NucleotideConverter(object):
         """
         Reads in the source and destination sequences.
         """
-        src = SeqIO.read(source, 'fasta', alphabet=DNAAlphabet)
-        des = SeqIO.read(destination, 'fasta', alphabet=DNAAlphabet)
+        src = SeqIO.read(source, 'fasta', alphabet=generic_dna)
+        des = SeqIO.read(destination, 'fasta', alphabet=generic_dna)
 
-        assert len(src.seq) == len(des.seq),
-        "Source and destination sequences must be of the same length!"
+        assert len(src.seq) == len(des.seq)
 
         self.source = src
         self.destination = des
 
-    def diff_codon_positions(self):
+    def compute_diff_codon_positions(self):
         """
         Sets the self.codon_positions attribute to a set of codon_positions
         where source sequence and destination sequence differ.
         """
 
-        assert isinstance(self.source, Bio.SeqRecord)
-
         # Initialize an empty set of codon positions
         codon_positions = set()
         for i, _ in enumerate(self.source.seq[::3]):
-            codon1 = self.source.seq[i * 3:i * 3 + 3]
-            codon2 = self.destination.seq[i * 3:i * 3 + 3]
-            if codon1 != codon2:
+            codon1 = self.source.seq[(i * 3):(i * 3 + 3)]
+            codon2 = self.destination.seq[(i * 3):(i * 3 + 3)]
+            if str(codon1) != str(codon2):
                 codon_positions.add(i)
 
         self.codon_positions = codon_positions
 
-    def distance_graph(self):
+    def compute_distance_graph(self):
         """
         Computes the distance between each pair of codons. They should be at
         least 50 codons (i.e. 150 n.t.) apart for them to be joined together.
         """
 
-        assert isinstance(self.positions, set),
-        "Compute the set of different positions first!"
+        assert isinstance(self.codon_positions, set)
 
         # Initialize a networkx directed graph.
         G = nx.DiGraph()
 
         # Iterate over every pair of positions. If they are greater than 50
         # codons apart, draw an edge between them.
-        for p1, p2 in combinations(self.positions, 2):
+        for p1, p2 in combinations(self.codon_positions, 2):
             dist = abs(p2 - p1)
             if dist > 50:
-                G.add_edge(p1, p2, dist=dist)
+                G.add_edge(min(p1, p2), max(p1, p2), dist=dist)
 
         self.distance_graph = G
 
-    def assembly_steps(self):
+    def compute_assembly_steps(self):
         """
         Computes the mutation assembly steps based on the distance graph.
 
@@ -97,8 +93,7 @@ class NucleotideConverter(object):
             - greedily add downstream until no more destination nodes
         """
         # Check first to make sure that the distance_graph has been made.
-        assert isinstance(self.distance_graph, nx.DiGraph),
-        "Compute the distance graph first!"
+        assert isinstance(self.distance_graph, nx.DiGraph)
 
         # Copy the distance graph.
         G = self.distance_graph.copy()
@@ -135,4 +130,5 @@ class NucleotideConverter(object):
             G.remove_nodes_from(protocol[i])
 
         self.protocol = protocol
-        
+
+
--- tests/test_nucleotide_converter.py
+++ tests/test_nucleotide_converter.py
@@ -1,5 +1,36 @@
-from FluGibson import nucleotide_converter
+from FluGibson.nucleotide_converter import NucleotideConverter
+from collections import defaultdict
+import os
+
+# Get the directory of this test file.
+package_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(os.path.join(package_directory, 'test_data'))
+
+nc = NucleotideConverter()
+nc.read_sequences('vic_np_mut1.fasta', 'vic_np_mut2.fasta')
+nc.compute_diff_codon_positions()
+nc.compute_distance_graph()
+nc.compute_assembly_steps()
+
+for i, _ in enumerate(nc.source.seq[::3]):
+    codon1 = nc.source.seq[(i * 3):(i * 3 + 3)]
+    codon2 = nc.destination.seq[(i * 3):(i * 3 + 3)]
+    if str(codon1) != str(codon2):
+        print(i, codon1, codon2)
+
+print(nc.codon_positions)
+print(nc.distance_graph.edges())
+print(nc.protocol)
 
 
-def test_diff_codon_positions():
-    pass
+def test_compute_distance_graph():
+    assert len(nc.distance_graph.edges()) == 5
+
+def test_compute_assembly_steps():
+    true_protocol = defaultdict(set)
+    true_protocol[1] = set([26, 362, 223])
+    true_protocol[2] = set([224])
+    assert nc.protocol == true_protocol
+
+def test_compute_diff_codon_positions():
+    assert nc.codon_positions == set([224, 26, 362, 223])
