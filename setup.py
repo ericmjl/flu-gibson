11aa83e786dcd9dfaf5b9f272af02ba233e43e3e
Eric Ma <ericmajinglong@gmail.com>
2015-08-12 12:41:01 -0400

    Setup script fixes

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     requires=['networkx', 'pandas', 'matplotlib', 'biopython'],
+    package_dir={'FluGibson':'FluGibson'},
     package_data={'readme': ['README.md'],
-                  'plasmids': ['FluGibson/plasmid_backbones/*.fasta']},
+                  'FluGibson': ['plasmid_backbones/*.fasta']},
 
 )
