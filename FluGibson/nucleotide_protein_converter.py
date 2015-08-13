6762ff035da391d9c5011dd990d1e12e7a4e601c
Eric Ma <ericmajinglong@gmail.com>
2015-08-13 16:13:34 -0400

    Added nucleotide protein converter class

---
+++ FluGibson/nucleotide_protein_converter.py
@@ -0,0 +1,9 @@
+class NucleotideProteinConverter(object):
+    """
+    A class that performs nucleotide to protein conversion.
+    """
+    def __init__(self, arg):
+        super(NucleotideProteinConverter, self).__init__()
+        self.arg = arg
+        
+
