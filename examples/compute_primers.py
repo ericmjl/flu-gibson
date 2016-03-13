c70b2e595898df306c37a4506a53140be9a94f44
Eric Ma <ericmajinglong@gmail.com>
2016-03-12 21:55:01 -0500

    bug fixes

--- examples/compute_primers.py
+++ examples/compute_primers.py
@@ -7,22 +7,28 @@ master_primer_list = list()
 
 for f in os.listdir(os.getcwd()):
     if '.fasta' in f:
-        p = PrimerDesigner(f)
+        p = PrimerDesigner()
+        p.read_sequences(f)
+        p.set_sequences(p.sequences)
         p.construct_graph()
         p.design_assembly_primers()
-        p.design_sequencing_primers()
+        p.design_junction_sequencing_primers()
+        p.design_fragment_sequencing_primers()
         p.compute_pcr_protocol()
         p.save_pcr_protocol()
 
         graph = p.graph
 
         for n, d in graph.nodes(data=True):
-            for prefix in ['fw', 're']:
+            print(n, d)
+            for direction in ['fw', 're']:
                 primer_data = dict()
-                primer_data['primer'] = d[
-                    '{prefix}_primer'.format(prefix=prefix)]
-                primer_data['sequence'] = d[
-                    '{prefix}_sequence'.format(prefix=prefix)].seq
+                primer_data['{0}_junction_primer'.format(direction)] = d['{0}_sequence'.format(direction)].seq
+                primer_data['{0}_sequencing_primer'.format(direction)] = d['{0}_sequencing_primer'.format(direction)].seq
+
+                primer_data['part'] = n.id
+                primer_data['{0}_junction_primer_name'.format(direction)] = '{0}_{1}_junction'.format(direction, n.id)
+                primer_data['{0}_seuqencing_primer_name'.format(direction)] = '{0}_{1}_sequencing'.format(direction, n.id)
 
                 master_primer_list.append(primer_data)
 
